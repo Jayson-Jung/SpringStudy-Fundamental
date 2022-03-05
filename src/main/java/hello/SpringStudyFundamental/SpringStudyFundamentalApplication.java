6b5afd38875826580a78c88d84d8d3a60a90546a
jayson-jung <hyeonsuns@kakao.com>
2022-03-05 21:47:27 +0900

    Initial commit

---
+++ src/main/java/hello/SpringStudyFundamental/SpringStudyFundamentalApplication.java
@@ -0,0 +1,13 @@
+package hello.SpringStudyFundamental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudyFundamentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudyFundamentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/SpringStudyFundamental/SpringStudyFundamentalApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.SpringStudyFundamental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyFundamentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
